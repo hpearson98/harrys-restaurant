7b267a6b64af5692ef4f2d1b5eef1a7dc1562e8a
hpearson98 <harry.m.pearson@gmail.com>
2023-08-03 17:36:57 +0000

    Created Customer, Table and Reservation model and migrated them

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_first_name")
+    last_name = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_last_name")
+    email = models.EmailField()
+
+
+class Table(models.Model):
+    seats = models.IntegerField()
+    min_people = models.IntegerField()
+    max_people = models.IntegerField()
+
+
+class Reservation(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE, related_name="reserved_table")
+    party = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="booking_name")
+    slot = models.DateField()
