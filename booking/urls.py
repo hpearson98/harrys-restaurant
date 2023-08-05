b2fa11e9a854d8c975d41ab69cea5f4eef9d6e52
hpearson98 <harry.m.pearson@gmail.com>
2023-08-05 14:56:24 +0000

    Fixed issue with html template not rendering

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('bookings.html', views.ReservationList.as_view(), name='bookings'),
+    path("", TemplateView.as_view(template_name="index.html")),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Reservation
+from django.views.generic import TemplateView
 
 
 # From 'View Creation Checklist' Code Institute video
@@ -8,3 +9,7 @@ class ReservationList(generic.ListView):
     model = Reservation
     queryset = Reservation.objects.order_by('date')
     template_name = 'bookings.html'
+
+
+class HomeView(TemplateView):
+    template_name = "index.html"
